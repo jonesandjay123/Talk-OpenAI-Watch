65265dbc49f4acbe41002e565d2179d8ba0637a7
joneswang <jonescwang@gmail.com>
2023-03-30 14:35:17 -0400

    both Chinese and English are now able to be voice input

--- app/src/main/java/com/example/talkopenaiwatch/MainActivity.kt
+++ app/src/main/java/com/example/talkopenaiwatch/MainActivity.kt
@@ -4,12 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
-import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
 import android.widget.TextView
-import androidx.core.content.pm.PackageInfoCompat
 import com.example.talkopenaiwatch.databinding.ActivityMainBinding
 import java.util.Locale
 
@@ -17,7 +15,6 @@ class MainActivity : Activity(), TextToSpeech.OnInitListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var textToSpeech: TextToSpeech
-    private val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +25,8 @@ class MainActivity : Activity(), TextToSpeech.OnInitListener {
         // 將版本號碼設置為 TextView
         val versionTextView = findViewById<TextView>(R.id.version_text)
         val packageInfo = packageManager.getPackageInfo(packageName, 0)
-        val versionCode = PackageInfoCompat.getLongVersionCode(packageInfo)
-        versionTextView.text = "版本 $versionCode"
+        val versionName = packageInfo.versionName
+        versionTextView.text = "版本 $versionName"
 
         // 為根視圖添加單擊監聽器
         binding.root.setOnClickListener { onScreenTapped(it) }
@@ -40,7 +37,7 @@ class MainActivity : Activity(), TextToSpeech.OnInitListener {
     private fun onScreenTapped(view: View) {
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
             putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-            putExtra(RecognizerIntent.EXTRA_PROMPT, "Say your question")
+            putExtra(RecognizerIntent.EXTRA_PROMPT, "請說出您的問題")
             putExtra(RecognizerIntent.EXTRA_LANGUAGE, "zh-TW") // 繁體中文
         }
         startActivityForResult(intent, 100)
@@ -67,8 +64,19 @@ class MainActivity : Activity(), TextToSpeech.OnInitListener {
                 val recognizedText = results[0]
                 binding.text.text = recognizedText
 
-                // 朗讀文字
-                textToSpeech.speak(recognizedText, TextToSpeech.QUEUE_FLUSH, null, "")
+                // 根據識別出的語言更新 TextToSpeech 的語言設置
+                val locale = if (recognizedText.matches(Regex("[\\u4E00-\\u9FA5]+"))) {
+                    Locale.TAIWAN
+                } else {
+                    Locale.US
+                }
+                textToSpeech.language = locale
+
+                // 使用 postDelayed 來給語音識別一點時間完成處理
+                binding.text.postDelayed({
+                    // 朗讀文字
+                    textToSpeech.speak(recognizedText, TextToSpeech.QUEUE_FLUSH, null, "")
+                }, 500) // 延遲 500 毫秒
             }
         }
     }
@@ -81,6 +89,5 @@ class MainActivity : Activity(), TextToSpeech.OnInitListener {
         }
 
         super.onDestroy()
-        speechRecognizer.destroy()
     }
 }
